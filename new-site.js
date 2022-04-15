2cb8978a839dcfdde381578475246a4abd4fbf5f
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-04-15 10:31:13 -0500

    Create new-site.js

---
+++ new-site.js
@@ -0,0 +1,3 @@
+// for the new site
+// by jax ;) happy unblocking
+javascript:document.body.innerHTML = "<iframe src=\"https:\/\/z99.herokuapp.com\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
