6550b89bb06d4333d7edc38fbcd4112c9afcbb68
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-04-15 13:18:17 -0500

    Update new-site.js

--- new-site.js
+++ new-site.js
@@ -1,3 +1,3 @@
 // for the new site
 // by jax ;) happy unblocking
-javascript:document.body.innerHTML = "<iframe src=\"https:\/\/unbl0ck3d.github.io/bookmarklets/menu.html\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
+javascript:document.body.innerHTML = "<iframe src=\"https:\/\/unbl0ck3d.github.io/bookmarklets/\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";
