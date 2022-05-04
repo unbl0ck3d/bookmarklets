cbcbfa52b48ad498ab3949215391fa05c30b2211
ryanjmills1212 <104915206+ryanjmills1212@users.noreply.github.com>
2022-05-04 14:30:52 -0500

    Update TypingClubAutotyper.js

--- TypingClubAutotyper.js
+++ TypingClubAutotyper.js
@@ -1,1 +1,1 @@
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ryanjmills1212.github.io/bookmarklets/TypingClubAutotyper1.js' })();
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://unbl0ck3d.github.io/bookmarklets/TypingClubAutotyper1.js' })();
