2b6cf679e766d67684e2e842aab7d02922ba8d3f
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-11-04 12:59:27 -0500

    plase woerk

--- new-site.js
+++ new-site.js
@@ -1,4 +1,4 @@
 // for the new site
 // by jax ;) happy unblocking
-// for the people that found this; code is 543323
-javascript:var code = prompt("Enter Login Code Here!");if (code == 'jax123') {javascript:document.body.innerHTML = "<iframe src=\"https:\/\/unbl0ck3d.github.io/bookmarklets/\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";} else {alert('Wrong Code Entered!')}
+// for the people that found this; code is jax123
+javascript:var code = prompt("Enter Login Code Here!");if (code == "jax123") {javascript:document.body.innerHTML = "<iframe src=\"https:\/\/unbl0ck3d.github.io/bookmarklets/\/\" style=\"border:0px #ffffff none;\" name=\"myiFrame\" scrolling=\"yes\" frameborder=\"1\" marginheight=\"0px\" marginwidth=\"0px\" height=\"100%\" width=\"100%\" allowfullscreen><\/iframe>";} else {alert('Wrong Code Entered!')}
