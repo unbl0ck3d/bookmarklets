a31530336516aaaf22386bdb9aa5dfb455711bfd
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-11-14 13:57:43 -0600

    add more names

--- title.js
+++ title.js
@@ -1,4 +1,4 @@
-var titles = ['pizza', 'fortnite', 'joe', 'fort', 'ejri', 'fjdisoj', 'ifodjsof', 'bfd']
+var titles = ['fortnite hacks free 2019', 'club penguin infinite coin hack 2012', 'joes epic gaming website', 'google.net', 'lightspeed bypass', 'schoology', 'the backrooms', 'your moms house']
 var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
 document.title = title;
 document.getElementById('my-span').innerHTML = title;
