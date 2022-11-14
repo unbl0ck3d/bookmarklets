ae1db038631656fa13dd70d586b6b38cd90035c8
Jax <61421101+JaxGamez@users.noreply.github.com>
2022-11-14 13:52:30 -0600

    please work

--- title.js
+++ title.js
@@ -1,3 +1,4 @@
 var titles = ['pizza', 'fortnite', 'joe', 'fort', 'ejri', 'fjdisoj', 'ifodjsof', 'bfd']
 var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
 document.title = title;
+document.getElementById('my-span').innerHTML = title;
